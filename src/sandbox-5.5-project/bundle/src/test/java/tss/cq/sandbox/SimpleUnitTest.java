5cf84188d0f2fd2885bc224116d19ada64328a24
Tedd Scofield <teddscofield@gmail.com>
2014-02-16 01:07:23 -0600

    add stock CQ archetype project

---
+++ src/sandbox-5.5-project/bundle/src/test/java/tss/cq/sandbox/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package tss.cq.sandbox;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
